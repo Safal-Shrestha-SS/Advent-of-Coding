9a606fa12d0250febec77391ed6314b74b04dc5e
Safal Shrestha <safalshrestha721156@gmail.com>
2024-12-11 20:39:26 +0545

    day 11 done

--- lib/days/11/day_11_2.dart
+++ lib/days/11/day_11_2.dart
@@ -1,33 +1,50 @@
 import 'package:advent_of_coding/file_reader.dart';
 
+// The Historians sure are taking a long time. To be fair, the infinite corridors are very large.
+
+// How many stones would you have after blinking a total of 75 times?
 void main() {
   var listOfString = readFile('lib/days/11/day_11_2.txt');
   List<String> noList = listOfString[0].split(' ');
 
   print(noList);
   Stopwatch stopwatch = Stopwatch()..start();
-  const noOfBlinks = 75;
+  const noOfBlinks = 25;
+
   int count = 0;
   for (var i = 0; i < noList.length; i++) {
-    count += listLength(noList[i], noOfBlinks);
+    count += listLength(int.parse(noList[i]), noOfBlinks);
   }
-  // Use a fixed-length list for performance optimization
+
   print(count);
-  print('do() execution took ${stopwatch.elapsed}');
+  // print(cache);
+  print('Execution took ${stopwatch.elapsed}');
 }
 
-int listLength(String listValue, int step) {
-  if (step == 0) {
-    return 1;
+Map<String, int> cache = {};
+
+int listLength(int listValue, int step) {
+  if (step == 0) return 1;
+
+  String key = '$listValue,$step';
+
+  if (!cache.containsKey(key)) {
+    int result = 0;
+
+    if (listValue == 0) {
+      result = listLength(1, step - 1);
+    } else if (listValue.toString().length % 2 == 0) {
+      String str = listValue.toString();
+      int mid = str.length ~/ 2;
+
+      result += listLength(int.parse(str.substring(0, mid)), step - 1);
+      result += listLength(int.parse(str.substring(mid)), step - 1);
+    } else {
+      result = listLength(listValue * 2024, step - 1);
+    }
+
+    cache[key] = result;
   }
-  if (listValue == '0') {
-    return listLength('1', step - 1);
-  }
-  if (listValue.length % 2 == 0) {
-    return listLength(listValue.substring(0, listValue.length ~/ 2), step - 1) +
-        listLength(
-            int.parse(listValue.substring(listValue.length ~/ 2)).toString(),
-            step - 1);
-  }
-  return listLength((int.parse(listValue) * 2024).toString(), step - 1);
+
+  return cache[key]!;
 }
